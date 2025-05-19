da6a449311bfaee32610af3b5b9500ff19d930f4
Topan Alfarisi <topan.alfa@gmail.com>
2025-05-19 16:40:34 +0700

    feat: change font style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistSans = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Outfit({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} antialiased`}
       >
         {children}
       </body>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}; 
